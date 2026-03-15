fbaf7614174eb561b6a2b558e28584448c3b5ff2
Fernando Pastor <fpastor@icloud.com>
2026-03-15 16:29:22 +0100

    feat: add Vite alias configuration for improved module resolution

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from "astro/config";
+import path from "path";
 
 export default defineConfig({
   site: "https://fpastor.github.io",
   compressHTML: true,
   build: {
     inlineStylesheets: "auto",
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
+    },
   },
 });
